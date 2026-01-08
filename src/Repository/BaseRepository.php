12a49417baf1c2f4649caee5f78877cd2529e338
ayoub <ayoub.lahouegue@gmail.com>
2026-01-08 20:52:22 +0100

    Changement finale

--- src/Repository/BaseRepository.php
+++ src/Repository/BaseRepository.php
@@ -8,10 +8,9 @@ class BaseRepository extends EntityRepository
 {
     public function findOneBy(array $criteria, array $orderBy = null)
     {
-        // Fix for empty WHERE clause
+        
         if (empty($criteria)) {
-            // Log the issue for debugging
-            // error_log('Empty findOneBy() called in ' . get_class($this));
+            
             return null;
         }
         
@@ -20,13 +19,12 @@ class BaseRepository extends EntityRepository
     
     public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
     {
-        // Fix for empty WHERE clause
+        
         if (empty($criteria)) {
-            // Log the issue for debugging
-            // error_log('Empty findBy() called in ' . get_class($this));
+            
             
             if ($limit) {
-                // Return limited results without WHERE
+                
                 $qb = $this->createQueryBuilder('e');
                 if ($orderBy) {
                     foreach ($orderBy as $field => $direction) {
